6464503e644fb2493cbaa82d962424fd2674e1ad
crmplusoficina-star <crmplus.oficina@gmail.com>
2026-08-21 10:26:32 -0300

    feat: ativa nova agenda multi-filial e filtros de periodo

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AppNoAuth from './AppNoAuth';
+import AppNoAuthV2 from './AppNoAuthV2';
 import './noauth.css';
+import './period.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AppNoAuth />
+    <AppNoAuthV2 />
   </React.StrictMode>,
 );
